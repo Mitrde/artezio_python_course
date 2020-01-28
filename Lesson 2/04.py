d9ea05da66f22ecc5a3aed8b2fa108e768d1c8d3
dotahub <dotahub@icloud.com>
2020-01-28 22:46:25 +0300

    Lesson 2 homework added

--- Lesson 2/04.py
+++ Lesson 2/04.py
@@ -3,7 +3,7 @@ def old_range(*args):
         return list(range(args[0]))
     elif len(args) == 2:
         return list(range(args[0], args[1]))
-    elif len(args) ==3:
+    elif len(args) == 3:
         return list(range(args[0], args[1], args[2]))
 
 
